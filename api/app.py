b01c0edf467007b538d01e83b8f56850d79a5c3f
Viswas Somapongo <viswass810@gmail.com>
2024-06-28 19:35:58 +0530

    second commit

--- api/app.py
+++ api/app.py
@@ -1,6 +1,8 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,send_from_directory
 from flask_cors import CORS
 from routes import main
+import os 
+
 
 app = Flask(__name__)
 CORS(app, origins='*')
@@ -11,6 +13,16 @@ app.register_blueprint(main)
 #     data = {"message": "Hello from Flask!"}
 #     return jsonify(data)
 
+
+client_folder = os.path.join(os.getcwd(),"..","client")
+dist_folder = os.path.join(client_folder,"dist")
+
+@app.route("/",defaults={"filename":""})
+@app.route("/<path:filename>")
+def index(filename):
+    if not filename:
+        filename = "index.html"
+    return send_from_directory(dist_folder,filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
